e25316f34535f46f267b54faf4da1204a10b7d18
Kateryna Akhynko <kateryna.akhynko@ucu.edu.ua>
2022-02-11 18:44:07 +0200

    reads file

--- main.py
+++ main.py
@@ -19,21 +19,41 @@ def parser_creation():
 def read_data(args):
     """Reads data from file and creates list with films of the
     needed year"""
-    films = []
     with open(args.path_to_data, 'r', encoding='utf-8') as file:
+        count =0
+        films = []
         for line in file:
             film = []
-            film.append()
+            line = line.split("\t")
+            
+            if args.year not in line:
+                continue
 
+            if count >= 14:    #not to include first lines of the file without important info
+                paren1_index = line[0].index('"')
+                paren2_index = line[0].index('" ')
+                film.append(line[0][paren1_index+2:paren2_index])
 
-def create_map(args):
-    new_map = folium.Map()
-    data = read_data(args)
-    
+                bracket1_index = line[0].index('(')
+                bracket2_index = line[0].index(')')
+                film.append(line[0][bracket1_index+1:bracket2_index])
 
-    map.save('your_map.html')
+                if "," not in line[-1]:
+                    film.append(line[-2].rstrip("\n"))
+                else:
+                    film.append(line[-1].rstrip("\n"))
+
+            count += 1
+
+            if len(film) != 0:
+                films.append(film)
+    return films
 
 
 if __name__ == '__main__':
     args = parser_creation()
-    create_map(args)
+    new_map = folium.Map()
+    data = read_data(args)
+    
+    
+    new_map.save('your_map.html')
