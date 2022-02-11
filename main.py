1e52241b9f710026dd315fcacf93a1419c941ca2
Kateryna Akhynko <kateryna.akhynko@ucu.edu.ua>
2022-02-12 01:32:40 +0200

    finished

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def read_data(year, path_to_data):
                     if '"' in line[0]:
                         paren1_index = line[0].index('"')
                         paren2_index = line[0].index('" ')
-                        film.append(line[0][paren1_index+1:paren2_index])
+                        film.append(line[0][paren1_index+2:paren2_index])
                     else:
                         film.append(line[0][:bracket1_index-1])
 
@@ -51,12 +51,11 @@ def read_data(year, path_to_data):
 
             count += 1
 
-            # if year not in line:
-            #     continue
+            if year not in film:
+                continue
 
             if len(film) != 0:
                 films.append(film)
-    print(films)
     return films
 
 
@@ -73,19 +72,25 @@ def get_coordinates(data, geolocator):
 def get_distance(latitude, longitude, coords):
     """Gets distances from location of the place"""
     for num, _ in enumerate(coords):
-        coords[num].append(12734.889 * math.asin(math.sqrt((math.sin
-        ((coords[num][3][0]-latitude)/2)) ** 2
-        + math.cos(coords[num][3][0]) * math.cos(latitude)
-        * (math.sin((longitude-coords[num][3][1])/2)) ** 2)))
+        try:
+            coords[num].append(12734.889 * math.asin(math.sqrt((math.sin
+            ((coords[num][3][0]-latitude)/2)) ** 2
+            + math.cos(coords[num][3][0]) * math.cos(latitude)
+            * (math.sin((longitude-coords[num][3][1])/2)) ** 2)))
+        except IndexError:
+            continue
     return coords
 
 
 def marker_layers(distances1):
     "Creates a new layer on a map with markers of locations"
     layer = folium.FeatureGroup(name="closest locations")
-    for num, loc in enumerate(distances1):
-        new_map.add_child(layer.add_child(folium.Marker(location=[loc[num][3][0],\
-        loc[num][3][1]], popup=loc[num][0])))
+    for _, loc in enumerate(distances1):
+        try:
+            new_map.add_child(layer.add_child(folium.Marker(location=[loc[3][0], \
+            loc[3][1]], popup=loc[0])))
+        except IndexError:
+            continue
 
     my_layer = folium.FeatureGroup(name="Lviv location")
     new_map.add_child(my_layer.add_child(folium.Marker(location=[49.8397, 24.0297], popup='Lviv')))
@@ -104,7 +109,10 @@ if __name__ == '__main__':
 
     coords1 = get_coordinates(data1, geolocator1)
     distances = get_distance(latitude1, longitude1, coords1)
-    distances = sorted(distances, key=lambda x: x[4])[:9]
+    try:
+        distances = sorted(distances, key=lambda x: x[4])[:9]
+    except IndexError:
+        pass
     marker_layers(distances)
 
     new_map.add_child(folium.LayerControl())
